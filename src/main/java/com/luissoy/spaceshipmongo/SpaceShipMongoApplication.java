9151bb73cdd7708784762a54c86fb2b2bc0df15f
luissoy <luisserranomarin@gmail.com>
2024-02-26 21:04:21 +0100

    feat: Initial commit

---
+++ src/main/java/com/luissoy/spaceshipmongo/SpaceShipMongoApplication.java
@@ -0,0 +1,13 @@
+package com.luissoy.spaceshipmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceShipMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceShipMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luissoy/spaceshipmongo/SpaceShipMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissoy.spaceshipmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceShipMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
